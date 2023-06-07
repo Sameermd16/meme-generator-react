e56d068da75fa982c23dad0da7632c753e707be4
sameermd16 <sameerthemc@gmail.com>
2023-06-07 18:03:59 +0530

    memeImages with useState

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,28 @@
-import react from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import memesData from '../memesData'
 
 
 
  function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        setMemeImage(memesArray[randomNumber].url);
+    }
+
     return (
         <Main>
-            <Form className='form'>
+            
+            <Div className='form'>
                 <input type='text' placeholder='top text' />
                 <input type='text' placeholder='bottom text' />
-                <Button className='meme-button'>Get a new meme image</Button>
-            </Form>
+                <Button className='meme-button' onClick={getMemeImage} >Get a new meme image</Button>
+            </Div>
+            <Img src={memeImage} className='meme-img' />
         </Main>
     )
 }
@@ -26,12 +38,18 @@ const Button = styled.button`
     color: white;
     background: linear-gradient(90deg, #672280 1.18%, #A626D3 100% );
     grid-column: 1 / -1;
+    cursor: pointer;
 `
 
-const Form = styled.form`
+const Div = styled.div`
    display: grid;
    grid-template: 40px 40px / 1fr 1fr;
    gap: 10px;
+`
+
+const Img = styled.img`
+    width: 100%;
+    height: 100%;
 `
 
 export default Meme;
